7aa5e8a9e12b1af3989e8a2c686755e87616de66
Ziming Lan <ziming.lan@gmail.com>
2017-04-25 20:14:04 +0800

    Milestone 3: Setting the stage for a continuous delivery workflow

--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -2,80 +2,88 @@
 
 var request = require('request');
 var dbSession = require('../../src/backend/dbSession.js');
+var Server = require('../../src/backend/server.js').Server;
 var resetDatabase = require('../resetDatabase.js');
 var async = require('async');
 
 describe('The API', function () {
 
+    var server;
 
-    it('should respond to a GET request at /api/keywords', function (done) {
+    beforeEach(function (done) {
+        server = Server('8081');
+        server.listen(function (err) {
+            resetDatabase(dbSession, function () {
+                done(err);
+            });
+        });
+    });
+
+    afterEach(function (done) {
+        server.close(function () {
+            resetDatabase(dbSession, function () {
+                done();
+            });
+        });
+    });
 
 
+    it('should respond to a GET request at /api/keywords/', function (done) {
         var expected = {
-            '_items': [
+            "_items": [
                 {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
                 {'id': 2, 'value': 'Onion', 'categoryID': 1},
                 {'id': 3, 'value': 'Knife', 'categoryID': 2}
             ]
         };
 
-        async.series([
+        async.series(
+            [
 
                 function (callback) {
-                    resetDatabase(dbSession, callback);
+                    dbSession.insert(
+                        'keyword',
+                        {'value': 'Aubergine', 'categoryID': 1},
+                        function (err) {
+                            callback(err)
+                        });
                 },
 
                 function (callback) {
-                    dbSession.insert('keyword', {
-                        'value': 'Aubergine',
-                        'categoryID': 1
-                    }, function (err) {
-                        callback(err);
-                    })
+                    dbSession.insert(
+                        'keyword',
+                        {'value': 'Onion', 'categoryID': 1},
+                        function (err) {
+                            callback(err)
+                        });
                 },
 
                 function (callback) {
-                    dbSession.insert('keyword', {
-                        'value': 'Onion',
-                        'categoryID': 1
-                    }, function (err) {
-                        callback(err);
-                    })
-                },
-
-                function (callback) {
-                    dbSession.insert('keyword', {
-                        'value': 'Knife',
-                        'categoryID': 2
-                    }, function (err) {
-                        callback(err);
-                    })
+                    dbSession.insert(
+                        'keyword',
+                        {'value': 'Knife', 'categoryID': 2},
+                        function (err) {
+                            callback(err)
+                        });
                 }
 
             ],
 
             function (err, results) {
-
-                request.get({
-                        'url': 'http://localhost:8080/api/keywords/',
+                if (err) throw(err);
+                request.get(
+                    {
+                        'url': 'http://localhost:8081/api/keywords/',
                         'json': true
                     },
                     function (err, res, body) {
-
                         expect(res.statusCode).toBe(200);
-
-                        // Note how expected is an object with a field _items, which contains
-                        // the actual list of keywords. Percolator doesn’t allow to return an
-                        // array of things directly.
                         expect(body).toEqual(expected);
                         done();
                     }
-                )
-
+                );
             }
         );
 
-
     });
-
 });
