c55b1ba9a3714a29143071b7fed55faea3c0c991
Ziming Lan <ziming.lan@gmail.com>
2017-04-25 19:18:01 +0800

    Milestone 2: API responds with actual database content

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,17 +1,31 @@
 'use strict';
 
 var Percolator = require('percolator').Percolator;
+var dbSession = require('../../src/backend/dbSession.js');
 
 var port = 8080;
 
-var server = Percolator({'port' : port});
+var server = Percolator({'port': port, 'autoLink': false});
 
 server.route('/api/keywords', {
-    GET: function(req, res) {
-        res.object({'foo' : 'bar'}).send();
+    GET: function (req, res) {
+
+        dbSession.fetchAll(
+            'SELECT id, value, categoryID FROM keyword ORDER by id',
+            function (err, rows) {
+
+                if (err) {
+                    console.log(err);
+                    res.status.internalServerError(err);
+                } else {
+                    res.collection(rows).send();
+                }
+            }
+        );
+
     }
 });
 
-server.listen(function() {
+server.listen(function () {
     console.log('Server started and listening on port', port);
 });
